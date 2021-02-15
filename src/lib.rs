7a9578ba419bc5379555b91810c1801d43c8eb98
Zach Peters <zpeters@gmail.com>
2021-02-15 09:27:49 -0600

    Added basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,41 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    use std::error::Error;
+     
+    pub struct Bugsnag{
+        pub apikey: String
     }
-}
+
+    impl Bugsnag {
+    fn notify(&self, level: &str, class: &str, msg: &str) -> Result<(),Box<dyn Error>> {
+        let resp = ureq::post("http://notify.bugsnag.com/")
+            .set("Content-Type", "application/json")
+            .set("Bugsnag-Api-Key", &self.apikey)
+            .set("Bugsnag-Payload-Version", "5")
+            .send_json(ureq::json!({
+                "notifier": {
+                    "name": "My Test Notifier",
+                    "version": "0.0.1",
+                    "url": "http://bcianswers.com"
+                },
+                "events": [
+                {"exceptions":[
+                    {
+                        "errorClass": class,
+                        "message": msg
+                    }
+                ]}
+                ],
+                "severity": level
+            }));
+        match resp {
+            Ok(_) => Ok(()),
+            Err(e) => return Err(Box::new(e)),
+        }
+    }
+
+    pub fn info(&self, class: &str, msg: &str) -> Result<(),Box<dyn Error>> {
+        self.notify("info", class, msg)
+    }
+    pub fn error(&self, class: &str, msg: &str) -> Result<(),Box<dyn Error>> {
+        self.notify("error", class, msg)
+    }
+    }
